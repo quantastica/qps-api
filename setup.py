99384628e6803cd86cfb91f9b8be44e8581eb49e
Petar Korponaić <petar.korponaic@gmail.com>
2021-07-29 01:18:38 +0300

    Version 0.9.5

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="quantastica-qps-api",
-    version="0.9.4",
+    version="0.9.5",
     description="Quantastica Quantum Programming Studio API",
     long_description=README,
     long_description_content_type="text/markdown",
